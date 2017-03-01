ee176edb861a897c9b17e9216b51c7bcdf56ac37
harrywan <harrywan@webank.com>
2017-03-01 10:39:01 +0800

    排序

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,25 +3,25 @@
         <h2>Pc组件</h2>
         <ul>
             <li> <a v-link="{ path:'/pc/layout'}">Layout</a> </li>
+            <li> <a v-link="{ path:'/pc/tab'}">Tab</a> </li>
             <li> <a v-link="{ path:'/pc/icon'}">Icon</a> </li>
+            <li> <a v-link="{ path:'/pc/zoom'}">Zoom</a> </li>
             <li> <a v-link="{ path:'/pc/banner'}">Banner</a> </li>
             <li> <a v-link="{ path:'/pc/table'}">Table</a> </li>
+            <li> <a v-link="{ path:'/pc/tree'}">Tree</a> </li>
+            <li> <a v-link="{ path:'/pc/step'}">Step</a> </li>
             <li> <a v-link="{ path:'/pc/toast'}">Toast</a> </li>
             <li> <a v-link="{ path:'/pc/message'}">Message</a> </li>
             <li> <a v-link="{ path:'/pc/modal'}">Modal</a> </li>
-            <li> <a v-link="{ path:'/pc/tree'}">Tree</a> </li>
-            <li> <a v-link="{ path:'/pc/datePicker'}">DatePicker</a> </li>
-            <li> <a v-link="{ path:'/pc/loading'}">Loading</a> </li>
-            <li> <a v-link="{ path:'/pc/switch'}">Switch</a> </li>
-            <li> <a v-link="{ path:'/pc/upload'}">Upload</a> </li>
-            <li> <a v-link="{ path:'/pc/zoom'}">Zoom</a> </li>
-            <li> <a v-link="{ path:'/pc/step'}">Step</a> </li>
             <li> <a v-link="{ path:'/pc/tooltip'}">Tooltip</a> </li>
-            <li> <a v-link="{ path:'/pc/tab'}">Tab</a> </li>
+            <li> <a v-link="{ path:'/pc/loading'}">Loading</a> </li>
             <li> <a v-link="{ path:'/pc/input'}">Input</a> </li>
             <li> <a v-link="{ path:'/pc/select'}">Select</a> </li>
             <li> <a v-link="{ path:'/pc/radio'}">Radio</a> </li>
             <li> <a v-link="{ path:'/pc/checkbox'}">Checkbox</a> </li>
+            <li> <a v-link="{ path:'/pc/datePicker'}">DatePicker</a> </li>
+            <li> <a v-link="{ path:'/pc/switch'}">Switch</a> </li>
+            <li> <a v-link="{ path:'/pc/upload'}">Upload</a> </li>
         </ul>
         <h2>Mobile组件</h2>
     </div>
